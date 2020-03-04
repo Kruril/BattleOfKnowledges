cb125f869e3a2228f1d0a156daeb8f4cbef66dc6
Mathieu Trempont <mathieu.trempont@hotmail.com>
2020-03-04 15:24:51 +0100

    Modif en UTF8

--- pGroupeB04/src/view/GamePage.java
+++ pGroupeB04/src/view/GamePage.java
@@ -1,16 +1,13 @@
 package view;
 
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.TextAlignment;
 import model.BackGroundLoader;
 
 public class GamePage extends BorderPane{
