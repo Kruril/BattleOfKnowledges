01ba5b6223e549db178694c4c95defaec9acdbd6
Mathieu Trempont <mathieu.trempont@hotmail.com>
2020-03-05 11:21:57 +0100

    Interface choix du thème

--- pGroupeB04/src/view/ChoiceThemeAP.java
+++ pGroupeB04/src/view/ChoiceThemeAP.java
@@ -73,21 +73,10 @@ public class ChoiceThemeGP extends GridPane{
 }*/
 package view;
 
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.stage.Screen;
 import model.BackGroundLoader;
 
 public class ChoiceThemeAP extends AnchorPane{
