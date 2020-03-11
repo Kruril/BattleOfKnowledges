d274ccef013108a509e17fe243f6eff66902a925
Mathieu Trempont <mathieu.trempont@hotmail.com>
2020-03-11 14:08:39 +0100

    Interface fin du jeu

---
+++ pGroupeB04/src/view/EndGameBP.java
@@ -0,0 +1,15 @@
+package view;
+
+import javafx.scene.layout.BorderPane;
+import model.BackgroundLoader;
+
+public class EndGameBP extends BorderPane{
+
+	
+	
+	public EndGameBP() {
+        //BACKGROUND
+        this.setBackground(BackgroundLoader.builderBackGround());
+	}
+	
+}
