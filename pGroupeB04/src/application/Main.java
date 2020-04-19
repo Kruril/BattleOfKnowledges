77ed4783e68266ae62c422a51a63814508f33c23
Kruril <misteurlova10@gmail.Com>
2020-04-19 19:05:38 +0200

    correction error SQL connection v2

--- pGroupeB04/src/application/Main.java
+++ pGroupeB04/src/application/Main.java
@@ -37,6 +37,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        SQLManager.getConnection();
         JsonManager.themeFromDeck();
         TrashManager.buildTrash();
         stage = primaryStage;
