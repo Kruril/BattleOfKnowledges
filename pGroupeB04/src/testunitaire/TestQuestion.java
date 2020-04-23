d9d1144249ded591c978cbe8db4412dbf3a1269a
valentinGailliez <63590566+valentinGailliez@users.noreply.github.com>
2020-04-23 17:37:30 +0200

    Test unitaire de questions presque termine

--- pGroupeB04/src/testunitaire/TestQuestion.java
+++ pGroupeB04/src/testunitaire/TestQuestion.java
@@ -12,17 +12,16 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import model.Question;
-import test.Explorateur;
 
 public class TestQuestion {
 
 	private static String author="test",theme="ThemeTeste",answer="Answer";
-	private static List<String> clue,listClues;
+	private static List<String>listClues;
 	private static String clue1="I'm a clue",clue2="I'm a second clue",clue3="I'm the last clue";
 	private static Question question;
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		clue=(List<String>) Explorateur.getField(question,"clues");
+		listClues=new ArrayList<>();
 	}
 
 	@AfterClass
@@ -37,14 +36,62 @@ public class TestQuestion {
 	public void tearDown() throws Exception {
 	}
 
+	/**
+	 * test checkQuestion method
+	 */
 	@Test
 	public void testCheckQuestion() {
-		clue.add(clue1);
-		clue.add(clue2);
-		clue.add(clue3);
-		question=new Question(author,theme,clue,answer);
+		listClues.add(clue1);
+		listClues.add(clue2);
+		listClues.add(clue3);
+		question=new Question(author,theme,listClues,answer);
 		question.checkQuestion();
-		assertTrue(clue.size()==3 && !(clue.contains("") && clue.contains(null)));
+		assertTrue(listClues.size()==3 && !(listClues.contains("") || listClues.contains(null)) && !(question.getAuthor().equalsIgnoreCase("")));
 	}
-		
+	
+	@Test
+	public void testCheckQuestionWith2Clues() {
+		listClues.add(clue1);
+		listClues.add(clue2);
+		question=new Question(author,theme,listClues,answer);
+		question.checkQuestion();
+		assertTrue(listClues.size()!=3);
+	}
+	@Test
+	public void testCheckQuestionWithoutAuthor() {
+		listClues.add(clue1);
+		listClues.add(clue2);
+		listClues.add(clue3);
+		author="";
+		question=new Question(author,theme,listClues,answer);
+		question.checkQuestion();
+		assertTrue(author.equalsIgnoreCase("") && listClues.size()==3);
+	}
+
+	@Test
+	public void testCheckQuestionWithToLongClue() {
+		listClues.add(clue1);
+		listClues.add(clue2);
+		clue3="hello. I am the third, last and longest index of the test but the maximum size of characters to be validated is one hundred and fifty. Anyway, I’m a clue.";
+		listClues.add(clue3);
+		question=new Question(author,theme,listClues,answer);
+		question.checkQuestion();
+		assertTrue(listClues.size()!=2 && author.equals(""));
+	}
+
+	
+	
+	/**
+	 * Test getClue method
+	 */
+	@Test
+	public void testGetClue() {
+		listClues.add(clue1);
+		listClues.add(clue2);
+		listClues.add(clue3);
+		question=new Question(author,theme,listClues,answer);
+		int index=0;
+		String clue=question.getClue(index);
+		assertTrue(clue1.equals(clue));
+	}
 }
