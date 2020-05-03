f70830774f3f521dcd5c47e4211f5826caffc225
Kruril <misteurlova10@gmail.Com>
2020-05-03 20:57:34 +0200

    continue after 4 point

---
+++ pGroupeB04/src/view/multiplayer/MultiPlayerRoomClient.java
@@ -0,0 +1,111 @@
+package view.multiplayer;
+
+import application.Main;
+import connection.Client;
+import connection.Server;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import utils.user.Player;
+import utils.utility.BackgroundLoader;
+
+import java.util.HashMap;
+
+public class MultiPlayerRoomClient extends StackPane {
+
+    private Label playerWait0, playerWait1, playerWait2, playerWait3;
+    private ImageView ivTitle;
+
+    private Button btnBack;
+
+    private Insets insets = new Insets(10.);
+
+    private HashMap<Integer, Label> playerWaits;
+
+    private Client client;
+
+    public MultiPlayerRoomClient(Client client) {
+        playerWaits = new HashMap<>();
+        this.client = client;
+
+        this.setBackground(BackgroundLoader.builderBackGround());
+
+        setAlignment(getIvTitle(), Pos.TOP_CENTER);
+
+        VBox vbWaitingPlayer = new VBox(getPlayerWait0(), getPlayerWait1(), getPlayerWait2(), getPlayerWait3());
+        vbWaitingPlayer.setMaxSize(720.,360.);
+        vbWaitingPlayer.setAlignment(Pos.CENTER);
+        vbWaitingPlayer.setSpacing(20.);
+        setAlignment(vbWaitingPlayer, Pos.CENTER);
+
+        setAlignment(getBtnBack(), Pos.BOTTOM_LEFT);
+
+        this.setPadding(new Insets(20.));
+        this.getChildren().addAll(getIvTitle(), vbWaitingPlayer, getBtnBack());
+        this.client.addHashMap(playerWaits);
+    }
+
+    public Label getPlayerWait0() {
+        if (playerWait0 == null) {
+            playerWait0 = new Label(Player.getName());
+            playerWait0.getStyleClass().add("waitingPlayer");
+            playerWait0.setPadding(insets);
+        }
+        return playerWait0;
+    }
+    public Label getPlayerWait1() {
+        if (playerWait1 == null) {
+            playerWait1 = new Label("Waiting player ...");
+            playerWait1.getStyleClass().add("waitingPlayer");
+            playerWait1.setPadding(insets);
+            playerWaits.put(1, playerWait1);
+        }
+        return playerWait1;
+    }
+
+    public Label getPlayerWait2() {
+        if (playerWait2 == null) {
+            playerWait2 = new Label("Waiting player ...");
+            playerWait2.getStyleClass().add("waitingPlayer");
+            playerWait2.setPadding(insets);
+            playerWaits.put(2, playerWait2);
+        }
+        return playerWait2;
+    }
+
+    public Label getPlayerWait3() {
+        if (playerWait3 == null) {
+            playerWait3 = new Label("Waiting player ...");
+            playerWait3.getStyleClass().add("waitingPlayer");
+            playerWait3.setPadding(insets);
+            playerWaits.put(3, playerWait3);
+        }
+        return playerWait3;
+    }
+
+    public ImageView getIvTitle() {
+        if (ivTitle == null) {
+            ivTitle = new ImageView(new Image("asset/images/title/Multiplayer_Room.png"));
+        }
+        return ivTitle;
+    }
+
+    public Button getBtnBack() {
+        if (btnBack == null) {
+            btnBack = new Button("Back");
+            btnBack.setBackground(BackgroundLoader.buildBtnBackGround());
+            btnBack.getStyleClass().add("buttonBasic");
+            btnBack.setId("small-button");
+            btnBack.setOnAction(event -> {
+                Main.switchScene(new JoinGame());
+                if (client != null) client.close();
+            });
+        }
+        return btnBack;
+    }
+}
---
+++ pGroupeB04/src/view/multiplayer/MultiPlayerRoomServer.java
@@ -0,0 +1,125 @@
+package view.multiplayer;
+
+import application.Main;
+import connection.Server;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import utils.user.Player;
+import utils.utility.BackgroundLoader;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+
+public class MultiPlayerRoomServer extends StackPane {
+
+    private Label playerWait0, playerWait1, playerWait2, playerWait3;
+    private ImageView ivTitle;
+
+    private Button btnBack, btnStart;
+
+    private Insets insets = new Insets(10.);
+
+    private Server server;
+
+    private HashMap<Integer, Label> playerWaits;
+
+    public MultiPlayerRoomServer() {
+        playerWaits = new HashMap<>();
+
+
+        this.setBackground(BackgroundLoader.builderBackGround());
+
+        setAlignment(getIvTitle(), Pos.TOP_CENTER);
+
+        VBox vbWaitingPlayer = new VBox(getPlayerWait0(), getPlayerWait1(), getPlayerWait2(), getPlayerWait3());
+        vbWaitingPlayer.setMaxSize(720.,360.);
+        vbWaitingPlayer.setAlignment(Pos.CENTER);
+        vbWaitingPlayer.setSpacing(20.);
+        setAlignment(vbWaitingPlayer, Pos.CENTER);
+
+        setAlignment(getBtnBack(), Pos.BOTTOM_LEFT);
+        setAlignment(getBtnStart(), Pos.BOTTOM_CENTER);
+
+        this.setPadding(new Insets(20.));
+        this.getChildren().addAll(getIvTitle(), vbWaitingPlayer, getBtnBack(), getBtnStart());
+
+        server = new Server(1234, playerWaits, getBtnStart());
+        server.start();
+    }
+
+    public Label getPlayerWait0() {
+        if (playerWait0 == null) {
+            playerWait0 = new Label(Player.getName());
+            playerWait0.getStyleClass().add("waitingPlayer");
+            playerWait0.setPadding(insets);
+        }
+        return playerWait0;
+    }
+    public Label getPlayerWait1() {
+        if (playerWait1 == null) {
+            playerWait1 = new Label("Waiting player ...");
+            playerWait1.getStyleClass().add("waitingPlayer");
+            playerWait1.setPadding(insets);
+            playerWaits.put(1, playerWait1);
+        }
+        return playerWait1;
+    }
+
+    public Label getPlayerWait2() {
+        if (playerWait2 == null) {
+            playerWait2 = new Label("Waiting player ...");
+            playerWait2.getStyleClass().add("waitingPlayer");
+            playerWait2.setPadding(insets);
+            playerWaits.put(2, playerWait2);
+        }
+        return playerWait2;
+    }
+
+    public Label getPlayerWait3() {
+        if (playerWait3 == null) {
+            playerWait3 = new Label("Waiting player ...");
+            playerWait3.getStyleClass().add("waitingPlayer");
+            playerWait3.setPadding(insets);
+            playerWaits.put(3, playerWait3);
+        }
+        return playerWait3;
+    }
+
+    public ImageView getIvTitle() {
+        if (ivTitle == null) {
+            ivTitle = new ImageView(new Image("asset/images/title/Multiplayer_Room.png"));
+        }
+        return ivTitle;
+    }
+
+    public Button getBtnBack() {
+        if (btnBack == null) {
+            btnBack = new Button("Back");
+            btnBack.setBackground(BackgroundLoader.buildBtnBackGround());
+            btnBack.getStyleClass().add("buttonBasic");
+            btnBack.setId("small-button");
+            btnBack.setOnAction(event -> {
+                server.shutdown();
+                Main.switchScene(new MultiplayerBP());
+            });
+        }
+        return btnBack;
+    }
+
+    public Button getBtnStart() {
+        if (btnStart == null) {
+            btnStart = new Button("Start game");
+            btnStart.setBackground(BackgroundLoader.buildBtnBackGround());
+            btnStart.getStyleClass().add("buttonBasic");
+            btnStart.setId("big-button");
+        }
+        return btnStart;
+    }
+}
